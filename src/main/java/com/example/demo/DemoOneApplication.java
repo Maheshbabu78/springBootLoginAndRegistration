4a8039f06ae9ccebed572dae902ff2501a979bb2
Maheshbabu <nadigotimahesh@gmail.com>
2022-09-02 10:17:15 +0530

    changes applicable

--- src/main/java/com/example/demo/DemoOneApplication.java
+++ src/main/java/com/example/demo/DemoOneApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//app start
 public class DemoOneApplication {
 
 	public static void main(String[] args) {
